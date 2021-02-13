6588b92df464b1ac25170e2a4a418b09849aac60
mooon <45768980+lublanc-dev@users.noreply.github.com>
2021-02-13 19:48:32 +0100

    Update firead.js

--- firead.js
+++ firead.js
@@ -9,22 +9,29 @@ var firebaseConfig = {
     measurementId: "X-XXXXXXK"
 };
 
+//Initialize your Firebase app
 firebase.initializeApp(firebaseConfig);
 
 console.log(firebase);
 
+//This listener will activate every time a new tab is opened
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
     var db = firebase.firestore();
+    //We ask the browser for the URL loaded and save it as "url_f"
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url_f = tabs[0].url;
-
+    
+    //Replace the "/" from the URL to ",". This step is needed in order to save the URL as a document in Firestore because Firebase
+    //doesn't admit "/", also, you can choose any other element to replace it.
     url_f = url_f.replaceAll("/", ",")
-
+    
+    //We get the reference of a pre-build collection from Firebase, in my case it was "URL"
     var docRef = db.collection("URL").doc(url_f);
     docRef.get().then(function(doc) {
         if (doc.exists) {
 
+          //The "JSON.stringify()" method is needed to show the data from the document
           window.alert(JSON.stringify(doc.data()))
 
 
